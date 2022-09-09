9b24e410beec3ee8495d7631ca2ec11f1c7d0b88
Candrawijay <112567644+Candrawijay@users.noreply.github.com>
2022-09-09 09:31:23 +0700

    Add files via upload

---
+++ 2217051020_candra wijaya_praktikum2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+	float a,c=3.14;
+	cout<<"menghitung luas lingkaran\n"<<"masukkan jari jari = ";
+	cin>>a;
+	cout<<"luas lingkarannya adalah "<<a*a*c<<" cm";
+}
